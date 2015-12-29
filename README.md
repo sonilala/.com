d5874313b53b6a8c47b698cbe091a8e0912f0516
Brandon Okeke <BrandonOkeke@gmail.com>
2015-12-29 09:04:06 +0000

    Updated margin styling

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 The source code for my personal website [Brawrdon.com](http://Brawrdon.com) which I use as my online portfolio.
 
 ## Screenshots
-Homepage: ![Home](http://brawrdon.com/assets/github/home.png)
+Home: ![Home](http://brawrdon.com/assets/github/home.png)
+
+Facts: ![Facts](http://brawrdon.com/assets/github/facts.png)
+
 
 ## Tools used
 * Brackets Text Editor
 * XAMPP (Widnows 10)
+* Google Fons
+* Google's Material Design icon set
 * A lot of Googling
 * My own free time and knowledge
--- facts.php
+++ facts.php
@@ -6,49 +6,56 @@
     <meta charset="UTF-8">
     <link href="../style.css" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,700' rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Random Facts &bull; Brawrdon</title>
 </head>
 
 <body>
     <nav>
         <ul>
-            <ul style="float:left;list-style-type:none;margin-left:100px;">
-                <li><a class="copy" href="/">&copy; Brawrdon 2016</a></li>
+            <ul style="float:left;margin-left:90px; margin-right:0;">
+                <li><a class="menu" href="/">&copy; Brawrdon 2016</a></li>
             </ul>
             <li><a class="menu" href="#">The Rest</a></li>
             <li><a class="menu" href="#">Media</a></li>
-            <li><a class="menu" href="#">Apps</a></li>
+            <li><a class="menu" href="#">Websites / Apps</a></li>
             <li><a class="menu" href="#">About</a></li>
             <li><a class="menu" href="/">Home</a></li>
         </ul>
     </nav>
-    <div class="jumbotron">
-        <div class="content">
-            <h1>Random Facts</h1>
-            <p>Here you'll find some facts about me that have been taken from a MySQL database and displayed onto the page with a fancy PHP script. Fancy right? Refresh the page for another one if you fancy! (Who said all these facts are about me huh?)</p>
-        </div>
-       
-    </div>
-    <div class="fact">
-        <h1>The random fact is:</h1>
-        <?php
-            require_once("scripts/connection.php");
-            $rand_id = rand(1, 11);
-            $sql = "SELECT * FROM ### WHERE ### = $rand_id";
-            $query = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($query) > 0) {
-                $fact_record = mysqli_fetch_assoc($query);
-                $fact_text = $fact_record["###"];  
-                echo "<div class=\"fact-text\"><div class=\"fact-wrapper\"><p>" . $fact_text . "</p></div></div>";
-            }
-            else {
-                echo "<p>There was an error</p>";
-            }
+    <div class="page-wrapper">
 
-            mysqli_close($conn);
-        ?>
+        <div class="jumbotron">
+            <div class="page-content">
+                <h1>Random Facts</h1>
+                <p>Here you'll find some facts, some about me and some other general ones. Either way they've all been taken from a fancy MySQL database and displayed onto the page with a fancy PHP script. Fancy right? Refresh the page for another one if you fancy!</p>
+            </div>
+        </div>
+        <div class="fact">
+            <div class="page-content">
+                <h1>The random fact is:</h1>
+            </div>
+            <div class="fact-text">
+                <div class="page-content">
+                    <?php
+                        require_once("scripts/connection.php");
+                        $rand_id = rand(1, 11);
+                        $sql = "SELECT * FROM ### WHERE ### = $rand_id";
+                        $query = mysqli_query($conn, $sql);
+                        if (mysqli_num_rows($query) > 0) {
+                            $fact_record = mysqli_fetch_assoc($query);
+                            $fact_text = $fact_record["###"];  
+                            echo "<p>" . $fact_text . "</p>";
+                        }
+                        else {
+                            echo "<p>There was an error... No fact for you</p>";
+                        }
+
+                        mysqli_close($conn);
+                    ?>
+                </div>
+            </div>
+        </div>
     </div>
 
 </body>
